cdd2a88b5291c161d2457e02d5a8801ae8542242
Ninja Tec <contato.esdrassantos@gmail.com>
2022-08-19 13:02:56 -0300

    Upgrade to POO
    Improvement on script characters.py to POO architecture.

--- Scripts/characters.py
+++ Scripts/characters.py
@@ -1,30 +1,42 @@
-letters_low = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-letters_upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+class Characters:
+    def __init__(self) -> None:
+        self.letters_low = [
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z'
+        ]
+        
+        self.letters_upper = [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+            'U', 'V', 'W', 'X', 'Y', 'Z'
+        ]
+        
+        self.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
-special_characters = ['!', '@', '#', '$', '%', '&', '*', '-', '=', ']', '[', '>',
-                      '<', '+', '?', '/', '{', ']', '[', '{', '.', ',', '|', ';']
+        self.special_characters = [
+            '!', '@', '#', '$', '%', '&', '*', '-', '=', ']',
+            '[', '>', '<', '+', '?', '/', '{', ']', '[', '{',
+            '.', ',', '|', ';'
+        ]
 
-chrt_letter_num = []
-for c in range(0, len(letters_upper) - 1):
-    chrt_letter_num.append(letters_upper[c])
-for c in range(0, len(letters_low) - 1):
-    chrt_letter_num.append(letters_low[c])
-for c in range(0, len(numbers) - 1):
-    chrt_letter_num.append(numbers[c])
+    def chrt_letter_num(self) -> list:
+        lista = []
+        lista += self.letters_upper 
+        lista += self.letters_low
+        lista += self.numbers
+        return lista
 
-all_chrt = []
-for c in range(0, len(special_characters) - 1):
-    all_chrt.append(special_characters[c])
-for c in range(0, len(chrt_letter_num) - 1):
-    all_chrt.append(chrt_letter_num[c])
-
-all_leters = []
-for c in range(0, len(letters_upper)-1):
-    all_leters.append(letters_upper[c])
-for c in range(0, len(letters_low)-1):
-    all_leters.append(letters_low[c])
+    def all_chrt(self) -> list:
+        lista = []
+        lista += self.special_characters
+        lista += self.chrt_letter_num()
+        return lista
+    
+    def all_leters(self) -> list:
+        lista = []
+        lista += self.letters_low
+        lista += self.letters_upper
+        return lista
--- Scripts/creator.py
+++ Scripts/creator.py
@@ -1,103 +1,103 @@
-import characters as ch
+from characters import Characters as ch
 from random import randint
 
 
 class CreatePassword:
-    def __init__(self, length, repeat=False):
+    def __init__(self, length, repeat=False) -> None:
         self.length = length
         self.repeat = repeat
 
-    def master_password(self):
+    def master_password(self) -> str:
         final_password = []
         repetition_counter = []
         while len(final_password) < self.length:
-            if self.length > len(ch.all_chrt) and self.repeat == False:
-                print(f"Error! Password must contain no more than {len(ch.all_chrt)} characters!")
+            if self.length > len(ch().all_chrt()) and self.repeat == False:
+                print(f"Error! Password must contain no more than {len(ch().all_chrt())} characters!")
                 break
             else:
-                raffled = randint(0, len(ch.all_chrt) - 1)
+                raffled = randint(0, len(ch().all_chrt()) - 1)
                 if self.repeat:
-                    final_password.append(ch.all_chrt[raffled])
+                    final_password.append(ch().all_chrt()[raffled])
                 else:
-                    if ch.all_chrt[raffled] in repetition_counter:
+                    if ch().all_chrt()[raffled] in repetition_counter:
                         pass
                     else:
-                        final_password.append(ch.all_chrt[raffled])
-                        repetition_counter.append(ch.all_chrt[raffled])
+                        final_password.append(ch().all_chrt()[raffled])
+                        repetition_counter.append(ch().all_chrt()[raffled])
         return final_password
 
     def numeric_password(self):
         final_password = []
         repetition_counter = []
         while len(final_password) < self.length:
-            if self.length > len(ch.numbers) and self.repeat == False:
-                print(f"Error! Password must contain no more than {len(ch.numbers)} characters!")
+            if self.length > len(ch().numbers) and self.repeat == False:
+                print(f"Error! Password must contain no more than {len(ch().numbers)} characters!")
                 break
             else:
-                raffled = randint(0, len(ch.numbers) - 1)
+                raffled = randint(0, len(ch().numbers) - 1)
                 if self.repeat:
-                    final_password.append(ch.numbers[raffled])
+                    final_password.append(ch().numbers[raffled])
                 else:
-                    if ch.numbers[raffled] in repetition_counter:
+                    if ch().numbers[raffled] in repetition_counter:
                         pass
                     else:
-                        final_password.append(ch.numbers[raffled])
-                        repetition_counter.append(ch.numbers[raffled])
+                        final_password.append(ch().numbers[raffled])
+                        repetition_counter.append(ch().numbers[raffled])
         return final_password
 
     def letter_password(self):
         final_password = []
         repetition_counter = []
         while len(final_password) < self.length:
-            if self.length > len(ch.all_leters) and self.repeat == False:
-                print(f"Error! Password must contain no more than {len(ch.all_leters)} characters!")
+            if self.length > len(ch().all_leters()) and self.repeat == False:
+                print(f"Error! Password must contain no more than {len(ch().all_leters())} characters!")
                 break
             else:
-                raffled = randint(0, len(ch.all_leters) - 1)
+                raffled = randint(0, len(ch().all_leters()) - 1)
                 if self.repeat:
-                    final_password.append(ch.all_leters[raffled])
+                    final_password.append(ch().all_leters()[raffled])
                 else:
-                    if ch.all_leters[raffled] in repetition_counter:
+                    if ch().all_leters()[raffled] in repetition_counter:
                         pass
                     else:
-                        final_password.append(ch.all_leters[raffled])
-                        repetition_counter.append(ch.all_leters[raffled])
+                        final_password.append(ch().all_leters()[raffled])
+                        repetition_counter.append(ch().all_leters()[raffled])
         return final_password
 
     def special_chrt_password(self):
         final_password = []
         repetition_counter = []
         while len(final_password) < self.length:
-            if self.length > len(ch.special_characters) and self.repeat == False:
-                print(f"Error! Password must contain no more than {len(ch.special_characters)} characters!")
+            if self.length > len(ch().special_characters) and self.repeat == False:
+                print(f"Error! Password must contain no more than {len(ch().special_characters)} characters!")
                 break
             else:
-                raffled = randint(0, len(ch.special_characters) - 1)
+                raffled = randint(0, len(ch().special_characters) - 1)
                 if self.repeat:
-                    final_password.append(ch.special_characters[raffled])
+                    final_password.append(ch().special_characters[raffled])
                 else:
-                    if ch.special_characters[raffled] in repetition_counter:
+                    if ch().special_characters[raffled] in repetition_counter:
                         pass
                     else:
-                        final_password.append(ch.special_characters[raffled])
-                        repetition_counter.append(ch.special_characters[raffled])
+                        final_password.append(ch().special_characters[raffled])
+                        repetition_counter.append(ch().special_characters[raffled])
         return final_password
 
     def letter_num_password(self):
         final_password = []
         repetition_counter = []
         while len(final_password) < self.length:
-            if self.length > len(ch.chrt_letter_num) and self.repeat == False:
-                print(f"Error! Password must contain no more than {len(ch.chrt_letter_num)} characters!")
+            if self.length > len(ch().chrt_letter_num()) and self.repeat == False:
+                print(f"Error! Password must contain no more than {len(ch().chrt_letter_num())} characters!")
                 break
             else:
-                raffled = randint(0, len(ch.chrt_letter_num) - 1)
+                raffled = randint(0, len(ch().chrt_letter_num()) - 1)
                 if self.repeat:
-                    final_password.append(ch.chrt_letter_num[raffled])
+                    final_password.append(ch().chrt_letter_num()[raffled])
                 else:
-                    if ch.chrt_letter_num[raffled] in repetition_counter:
+                    if ch().chrt_letter_num()[raffled] in repetition_counter:
                         pass
                     else:
-                        final_password.append(ch.chrt_letter_num[raffled])
-                        repetition_counter.append(ch.chrt_letter_num[raffled])
+                        final_password.append(ch().chrt_letter_num()[raffled])
+                        repetition_counter.append(ch().chrt_letter_num()[raffled])
         return final_password
